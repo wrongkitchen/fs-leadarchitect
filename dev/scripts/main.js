d397c4270f8108db83d7b1c22e0c93eac93b99bb
Kenji Wong <wkc.kenji@gmail.com>
2014-07-28 13:36:33 +0800

    update something

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -9,7 +9,39 @@ require.config({
 });
 
 require(['app'], function (app) {
-    app.init('Welcome to Yeogurt!');
-    console.log('Running jQuery %s', $().jquery);
+	$(document).ready(function(){
+
+		$("#termsCaller").on('click', function(){
+			if($('.terms').length){
+				$('.terms').slideDown();
+			}
+		});
+		$('.terms>.close').on('click', function(){
+			if($('.terms').length){
+				$('.terms').slideUp();
+			}
+		});
+
+
+		$('.about-node>.about-title').on('click', function(){
+			var slide = $(this).parent().children('.about-slide');
+			if(slide.css('display') == 'block'){
+				slide.slideUp();
+			} else {
+				slide.slideDown();
+			}
+		});
+
+		$(".fw-button>.txt").on('click', function(){
+			var fwButton = $(this).parent();
+			if(fwButton.hasClass('active')){
+				fwButton.removeClass('active');
+				fwButton.children('.footerSlide').slideUp();
+			} else {
+				fwButton.addClass('active');
+				fwButton.children('.footerSlide').slideDown();
+			}
+		});
+	});
 });
 
